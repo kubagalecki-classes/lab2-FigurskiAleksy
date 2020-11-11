5ad7f0449b64ccff1b0398bfce8786b4168b5e45
FigurskiAleksy <aleksy.figurski@gmail.com>
2020-11-11 21:40:54 +0000

    przenoszenie ulepszone

--- zal/ResourceManager.hpp
+++ zal/ResourceManager.hpp
@@ -19,4 +19,14 @@ class ResourceManager
       a= new Resource(*l.a));
     }//kopiowanie
 
+    ResourceManager(ResourceManager$$ l):a(l.a)
+    {
+      l.a=nullptr;
+    }//przenoszenie
+
+    ResourceManager& operator=(ResourceManager&& l)
+    {
+      i
+    }
+
 };
