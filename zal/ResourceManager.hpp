1b1a23adc428e6b1ed2a9b37719e8ab5710d525e
FigurskiAleksy <aleksy.figurski@gmail.com>
2020-11-11 21:37:30 +0000

    dodane kopiowanie

--- zal/ResourceManager.hpp
+++ zal/ResourceManager.hpp
@@ -11,5 +11,12 @@ class ResourceManager
     ResourceManager()
     {
       a=new Resource;
-    };
+    };//domyslny
+    
+
+    ResourceManager(const ResourceManager& l)
+    {
+      a= new Resource(*l.a));
+    }//kopiowanie
+
 };
